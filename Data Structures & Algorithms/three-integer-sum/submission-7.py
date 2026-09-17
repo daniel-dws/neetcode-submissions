58c0724bd70f5f783505909a795a016b50b8412d
daniel-dws <108303183+daniel-dws@users.noreply.github.com>
2026-09-17 09:42:27 -0700

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort() #nlogn
+        l = []
+
+        #max water approach
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum < 0:
+                    j+=1
+                elif sum > 0:
+                    k-=1
+                else:
+                    l.append([nums[i], nums[j], nums[k]])
+                    j+=1
+                    k-=1
+                    while j < k and nums[j] == nums[j-1]:
+                        j+=1
+        return l
+
+
+        
